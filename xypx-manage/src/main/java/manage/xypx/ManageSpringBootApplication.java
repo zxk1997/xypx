ebdb1fcfe2debf2f83d9a71a6d1f8ff9d22ecf80
97lulala <dnf2503@qq.com>
2019-03-17 10:44:49 +0800

    # WARNING: head commit changed in the meantime

--- xypx-manage/src/main/java/manage/xypx/ManageSpringBootApplication.java
+++ xypx-manage/src/main/java/manage/xypx/ManageSpringBootApplication.java
@@ -4,12 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan(basePackages="manage.xypx.DAO")//扫描所有映射接口
+@MapperScan(basePackages="manage.xypx.DAO")
 @SpringBootApplication
 public class ManageSpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ManageSpringBootApplication.class, args);
 	}
-
+   
 }
